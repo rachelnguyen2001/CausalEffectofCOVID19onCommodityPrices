2f960fb96016d8f3181874d6e8bda0fedd05c994
Rachel Nguyen <rmn1@williams.edu>
2021-12-18 23:09:25 -0500

    done

--- simd.c
+++ simd.c
@@ -12,11 +12,25 @@
 int hasChar(char* inArray, int numEntries) {
 	//fill in your solution here
 	//(unless you filled it in inline in the tex file)
+  for (int i = 0; i < numEntries / 32; i++) {
+    __m256i a = _mm256_loadu_si256((__m256i*)(inArray+32*i));
+    int integerResult = _mm256_movemask_epi8(a);
+
+    if (integerResult != 0) {
+      return 1;
+    }
+  }
+
+  return 0;
 }
 
 
 //this is just here in case it's useful
 int main() {
 	char test1[] = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 128, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
-	char test2[] = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 }; 
+	char test2[] = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+	char *t1 = test1;
+	char *t2 = test2;
+	printf("%d", hasChar(t1, 32));
+	printf("%d", hasChar(t2, 32));
 }
